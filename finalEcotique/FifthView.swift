cb9ca0c97496f16bc75cb35204de6fdddad622fa
Nanditha Sharath <nandithasharath@gmail.com>
2024-06-13 15:23:32 -0500

    Last day of final projects progress

---
+++ finalEcotique/FifthView.swift
@@ -0,0 +1,18 @@
+//
+//  FifthView.swift
+//  finalEcotique
+//
+//  Created by Scholar on 6/13/24.
+//
+
+import SwiftUI
+
+struct FifthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FifthView()
+}
---
+++ finalEcotique/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  finalEcotique
+//
+//  Created by Scholar on 6/13/24.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondView()
+}
