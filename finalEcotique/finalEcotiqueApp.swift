ee3867aeaed37a70b427c86c74c7ac7cdc23b8c2
Nanditha Sharath <nandithasharath@gmail.com>
2024-06-13 13:50:46 -0500

    Initial Commit

---
+++ finalEcotique/finalEcotiqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalEcotiqueApp.swift
+//  finalEcotique
+//
+//  Created by Scholar on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct finalEcotiqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
